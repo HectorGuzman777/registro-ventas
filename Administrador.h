62ced15f23301c9f4a6471b2c277d2b77afe706a
HectorGuzman777 <111295939+HectorGuzman777@users.noreply.github.com>
2023-11-12 11:13:22 -0600

    Update Administrador.h

--- Administrador.h
+++ Administrador.h
@@ -5,32 +5,27 @@
 #include <algorithm>
 #include <iostream>
 #include <cctype>  
+#include <fstream>
 
 struct Producto {
     int ventas;
     std::string nombre;
 };
-
-//compara las ventas de los productos
+//Compara los productos
 bool compararProductos(const Producto& a, const Producto& b) {
     return a.ventas > b.ventas;
 }
-
-//implementación de std::sort de la biblioteca de c++
+//ordena los prodecutos de mayor a menor utilizando la funcion std::sort de la libreria de c++
 void ordenar(std::vector<Producto>& productos) {
     std::sort(productos.begin(), productos.end(), compararProductos); 
 }
-
-//imprime los numeros que indican el lugar del producto segun su numero de ventas
+//funcion para darle formato
 void imprimirConNumeros(const std::vector<Producto>& productos) {
     for (int i = 0; i < productos.size(); ++i) {
         std::cout << i+1 << " ventas: " << productos[i].ventas << " Nombre: " << productos[i].nombre << std::endl;
     }
 }
-
-
-
-//modifica el numero de ventas de los productos
+//modifica la lista segun el numero de ventas que haya cambiado el usuario
 void modificar(std::vector<Producto>& productos, int lugar, int nuevasVentas) {
     if (lugar > 0 && lugar <= productos.size()) {
         productos[lugar - 1].ventas = nuevasVentas;
@@ -38,8 +33,20 @@ void modificar(std::vector<Producto>& productos, int lugar, int nuevasVentas) {
         std::cout << "Lugar no válido." << std::endl;
     }
 }
+//Crea un nuevo txt
+void escribirProductos(const std::vector<Producto>& productos) {
+    std::ofstream archivo("articulos_modificados.txt");
+    if (!archivo.is_open()) {
+        std::cerr << "No se pudo abrir el archivo para escritura." << std::endl;
+        return;
+    }
+    for (std::vector<Producto>::const_iterator it = productos.begin(); it != productos.end(); ++it) {
+        archivo << it->nombre << '-' << it->ventas << '\n';
+    }
+    archivo.close();
+}
 
+#endif
 
-#endif 
 
 
